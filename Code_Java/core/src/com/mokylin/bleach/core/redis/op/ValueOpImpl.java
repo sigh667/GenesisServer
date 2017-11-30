40e73e4b6c5df35d3d825743e876684179068299
baoliang.shen <85789685@qq.com>
2017-11-30 23:05:18 +0800

    修正Redis接口和单元测试

--- Code_Java/core/src/com/mokylin/bleach/core/redis/op/ValueOpImpl.java
+++ Code_Java/core/src/com/mokylin/bleach/core/redis/op/ValueOpImpl.java
@@ -1,100 +1,117 @@
 package com.mokylin.bleach.core.redis.op;
 
+import redis.clients.jedis.Client;
+import redis.clients.jedis.JedisPool;
+import redis.clients.util.SafeEncoder;
+
 import com.mokylin.bleach.core.redis.IRedisResponse;
 import com.mokylin.bleach.core.redis.op.actiononconnectfail.IActionOnCannotConnectRedis;
 import com.mokylin.bleach.core.serializer.ISerializer;
 import com.mokylin.bleach.core.serializer.ISerializerPool;
 
-import redis.clients.jedis.Client;
-import redis.clients.jedis.JedisPool;
-import redis.clients.util.SafeEncoder;
-
 /**
  * Redis中单值操作的实现。
- *
+ * 
  * @author pangchong
  *
  */
 public class ValueOpImpl extends AbstractOp implements IValueOp {
+	
+	public ValueOpImpl(JedisPool jedisPool, ISerializerPool serializerPool, IActionOnCannotConnectRedis actionOnFail) {
+		super(jedisPool, serializerPool, actionOnFail);
+	}
+	
+	public ValueOpImpl(IRedisCmdExecutor cmdOp){
+		super(cmdOp);
+	}
 
-    public ValueOpImpl(JedisPool jedisPool, ISerializerPool serializerPool,
-            IActionOnCannotConnectRedis actionOnFail) {
-        super(jedisPool, serializerPool, actionOnFail);
-    }
+	@Override
+	public IRedisResponse<String> set(final String key, final Object value) {
+		return cmdOp.execCommand(new SingleRedisCmd<String>() {
+			@Override
+			public void apply(Client jedis, ISerializer serializer) {
+				jedis.set(SafeEncoder.encode(key), serializer.serialize(value));
+			}
+		});
+	}
 
-    public ValueOpImpl(IRedisCmdExecutor cmdOp) {
-        super(cmdOp);
-    }
+	@Override
+	public IRedisResponse<String> setIfAbsent(final String key, final Object value) {
+		return cmdOp.execCommand(new SingleRedisCmd<String>() {
+			@Override
+			public void apply(Client jedis, ISerializer serializer) {
+				jedis.set(SafeEncoder.encode(key), serializer.serialize(value), SafeEncoder.encode("NX"));
+			}
+		});
+	}
 
-    @Override
-    public IRedisResponse<String> set(final String key, final Object value) {
-        return cmdOp.execCommand(new SingleRedisCmd<String>() {
-            @Override
-            public void apply(Client jedis, ISerializer serializer) {
-                jedis.set(SafeEncoder.encode(key), serializer.serialize(value));
-            }
-        });
-    }
+	@Override
+	public IRedisResponse<String> setIfPresent(final String key, final Object value) {
+		return cmdOp.execCommand(new SingleRedisCmd<String>() {
+			@Override
+			public void apply(Client jedis, ISerializer serializer) {
+				jedis.set(SafeEncoder.encode(key), serializer.serialize(value), SafeEncoder.encode("XX"));
+			}
+		});
+	}
 
-    @Override
-    public IRedisResponse<String> setIfAbsent(final String key, final Object value) {
-        return cmdOp.execCommand(new SingleRedisCmd<String>() {
-            @Override
-            public void apply(Client jedis, ISerializer serializer) {
-                jedis.set(SafeEncoder.encode(key), serializer.serialize(value),
-                        SafeEncoder.encode("NX"));
-            }
-        });
-    }
+	@Override
+	public IRedisResponse<String> setIfAbsentExpiredBySeconds(final String key, final Object value, final long second) {
+		return cmdOp.execCommand(new SingleRedisCmd<String>() {
+			@Override
+			public void apply(Client jedis, ISerializer serializer) {
+				jedis.set(SafeEncoder.encode(key), serializer.serialize(value),
+						SafeEncoder.encode("NX"), SafeEncoder.encode("EX"), second);
+			}
+		});
+	}
+	
+	@Override
+	public IRedisResponse<String> setIfPresentExpiredBySeconds(final String key, final Object value, final long second) {
+		return cmdOp.execCommand(new SingleRedisCmd<String>() {
+			@Override
+			public void apply(Client jedis, ISerializer serializer) {
+				jedis.set(SafeEncoder.encode(key), serializer.serialize(value),
+						SafeEncoder.encode("XX"), SafeEncoder.encode("EX"), second);
+			}
+		});
+	}
 
-    @Override
-    public IRedisResponse<String> setIfPresent(final String key, final Object value) {
-        return cmdOp.execCommand(new SingleRedisCmd<String>() {
-            @Override
-            public void apply(Client jedis, ISerializer serializer) {
-                jedis.set(SafeEncoder.encode(key), serializer.serialize(value),
-                        SafeEncoder.encode("XX"));
-            }
-        });
-    }
+	@Override
+	public IRedisResponse<String> setIfAbsentExpiredByMilliseconds(final String key, final Object value, final long millisenconds) {
+		return cmdOp.execCommand(new SingleRedisCmd<String>() {
+			@Override
+			public void apply(Client jedis, ISerializer serializer) {
+				jedis.set(SafeEncoder.encode(key), serializer.serialize(value),
+						SafeEncoder.encode("NX"), SafeEncoder.encode("PX"), millisenconds);
+			}
+		});
+	}
+	
+	@Override
+	public IRedisResponse<String> setIfPresentExpiredByMilliseconds(final String key, final Object value, final long millisenconds) {
+		return cmdOp.execCommand(new SingleRedisCmd<String>() {
+			@Override
+			public void apply(Client jedis, ISerializer serializer) {
+				jedis.set(SafeEncoder.encode(key), serializer.serialize(value),
+						SafeEncoder.encode("XX"), SafeEncoder.encode("PX"), millisenconds);
+			}
+		});
+	}
 
-    @Override
-    public IRedisResponse<String> setExpiredBySeconds(final String key, final Object value,
-            final long second) {
-        return cmdOp.execCommand(new SingleRedisCmd<String>() {
-            @Override
-            public void apply(Client jedis, ISerializer serializer) {
-                //                jedis.set(SafeEncoder.encode(key), serializer.serialize(value),
-                //                        SafeEncoder.encode("EX"), second);
-            }
-        });
-    }
-
-    @Override
-    public IRedisResponse<String> setExpiredByMilliseconds(final String key, final Object value,
-            final long millisenconds) {
-        return cmdOp.execCommand(new SingleRedisCmd<String>() {
-            @Override
-            public void apply(Client jedis, ISerializer serializer) {
-                //                jedis.set(SafeEncoder.encode(key), serializer.serialize(value),
-                //                        SafeEncoder.encode("PX"), millisenconds);
-            }
-        });
-    }
-
-    @Override
-    public <T> IRedisResponse<T> get(final String key, final Class<T> type) {
-        return cmdOp.execCommand(new SingleRedisCmd<T>() {
-            @Override
-            public void apply(Client jedis, ISerializer serializer) {
-                jedis.get(SafeEncoder.encode(key));
-            }
-
-            @Override
-            public T processResult(Object value, Client jedis, ISerializer serializer) {
-                return serializer.deserialize((byte[]) value, type);
-            }
-        });
-    }
+	@Override
+	public <T> IRedisResponse<T> get(final String key, final Class<T> type) {
+		return cmdOp.execCommand(new SingleRedisCmd<T>() {
+			@Override
+			public void apply(Client jedis, ISerializer serializer) {
+				jedis.get(SafeEncoder.encode(key));				
+			}
+			
+			@Override
+			public T processResult(Object value, Client jedis, ISerializer serializer){
+				return serializer.deserialize((byte[])value, type);
+			}
+		});		
+	}
 
 }
