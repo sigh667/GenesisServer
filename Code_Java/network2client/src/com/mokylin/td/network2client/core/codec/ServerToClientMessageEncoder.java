f5edb2afe4027e0b38f2fce428171f78b72a15da
Joey <85789685@qq.com>
2018-02-07 16:21:25 +0800

    对Client的编码解码修改

--- Code_Java/network2client/src/com/mokylin/td/network2client/core/codec/ServerToClientMessageEncoder.java
+++ Code_Java/network2client/src/com/mokylin/td/network2client/core/codec/ServerToClientMessageEncoder.java
@@ -17,7 +17,7 @@ import io.netty.handler.codec.MessageToByteEncoder;
  * | message body length | type | message body | <br>
  * +---------------------+------+--------------+ <br>
  * <br>
- * 1. 消息体的长度: 3 byte, 用于解析时从byte流中剥离消息;<br>
+ * 1. 消息体的长度: 4 byte, 用于解析时从byte流中剥离消息;<br>
  * 2. 消息号：2 byte, 用于指定具体的消息类型;<br>
  * 3. 消息体：长度由1指定, 具体的消息内容;<br>
  *
@@ -31,8 +31,7 @@ public class ServerToClientMessageEncoder extends MessageToByteEncoder<SCMessage
     protected void encode(ChannelHandlerContext ctx, SCMessage msg, ByteBuf out) throws Exception {
         int msgType = msg.messageType;
         byte[] content = msg.messageContent;
-        out.order(ByteOrder.LITTLE_ENDIAN).writeMedium(content.length).writeShort(msgType)
-                .writeBytes(content);
+        out.order(ByteOrder.LITTLE_ENDIAN).writeInt(content.length).writeShort(msgType).writeBytes(content);
     }
 
 }
